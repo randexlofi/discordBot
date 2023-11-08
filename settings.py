6e6d2168a2c0e0df4fc3d4c571633860f02fff07
randex <103509701+randexlofi@users.noreply.github.com>
2023-11-08 22:13:22 +0000

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # Tokens / Keys
-botToken = 'MTE2NTQxOTg0NjE4MDI5NDc3Ng.G5XDEZ.X3GdvPypXmZiztaqw2BOp1Lv5CuAwifwocFFAc'
-weatherAPIKey = '338ebfa074a6c17c9d1872b45230e82c'
+botToken = 'BOT_TOKEN'
+weatherAPIKey = 'YOUR_WEATHER_API'
 
 # Default Paramters
 defaultFunds = 100
